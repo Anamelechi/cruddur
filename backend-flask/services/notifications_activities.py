1165feb05ca33353c119119bb85a515d9e69adfc
Anamelechi <philznjoku@gmail.com>
2024-12-01 00:44:37 +0000

    add segments and subsegments

--- backend-flask/services/notifications_activities.py
+++ backend-flask/services/notifications_activities.py
@@ -1,7 +1,15 @@
 from datetime import datetime, timedelta, timezone
 class NotificationsActivities:
   def run():
+    with xray_recorder.in_segment('notifications_activities') as segment:
+
     now = datetime.now(timezone.utc).astimezone()
+dict = {
+"now" :now.isoformat()
+  }
+
+     segment.put_metadata('key', dict, 'namespace')
+
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
       'handle':  'PPK',
