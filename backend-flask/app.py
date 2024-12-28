81bd6f6d3d0cceee626d2e84ef4f59108c6f6213
Anamelechi <philznjoku@gmail.com>
2024-12-28 15:47:16 +0000

    implementing sentry

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from flask_cors import CORS, cross_origin
 import os
+from time import strftime
 
 from services.home_activities import *
 from services.user_activities import *
@@ -13,7 +14,58 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+# HoneyComb ---------
+from opentelemetry import trace
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter, SimpleSpanProcessor
+
+#Sentry
+import sentry_sdk
+from flask import Flask
+
+
+# CloudWatch Logs ----
+import watchtower
+import logging
+
+
+
+# Configuring Logger to Use CloudWatch
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+LOGGER.addHandler(console_handler)
+LOGGER.addHandler(cw_handler)
+LOGGER.info("some message")
+
+
+@app.after_request
+def after_request(response):
+    timestamp = strftime('[%Y-%b-%d %H:%M]')
+    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+    return response
+
+
+# Initialize tracing and an exporter that can send data to Honeycomb
+provider = TracerProvider()
+processor = BatchSpanProcessor(OTLPSpanExporter())
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__)
+
+
 app = Flask(__name__)
+#XRayMiddleware(app, xray_recorder)
+
+# HoneyComb Initialize automatic instrumentation with Flask
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
+
+
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
@@ -24,6 +76,25 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
+
+#Sentry 
+
+sentry_sdk.init(
+    dsn="https://db0e40dcaf18087a90c5d5fc20551c54@o4508444987097088.ingest.de.sentry.io/4508445037428816",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for tracing.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
+
+#SENTRY TEST
+@app.route('/sentry/test')
+def hello_world():
+    1/0  # raises an error
+    return "<p>Hello, World!</p>"
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
